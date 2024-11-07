7b97297d58188fd7f6af860dddec90ac251b27d4
Fuhad Hasan Shiblu <145831086+fuhad-shiblu@users.noreply.github.com>
2024-11-07 12:35:38 +0600

    Footer.jsx

--- src/assets/Components/Footer/Footer.jsx
+++ src/assets/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const Footer = () => {
   return (
     <>
-    <footer className="mt-10 text-xl font-light flex justify-center items-center gap-1">Created with <FaHeart className="text-[#ff2020]"/> by <Link className="font-black hover:underline" to={'https://github.com/fuhad-shiblu'}>Fuhad Hasan Shiblu</Link></footer>
+    <footer className="mt-10 text-xl font-light flex flex-wrap justify-center items-center gap-1">Created with <FaHeart className="text-[#ff2020]"/> by <Link className="font-black hover:underline" to={'https://github.com/fuhad-shiblu'}>Fuhad Hasan Shiblu</Link>| Build with <FaReact className="text-xl text-[#2ac3ff]"/><span className="font-bold">ReactJS</span></footer>
     </>
   )
 }
